e8c0791fd6919372e9b92d48752425f4629039d0
David C <david@learningequality.org>
2018-08-13 15:47:39 -0700

    Using pyjnius to find external storage Using pyjnius to find timezone

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
+from jnius import autoclass
 import os, sys
+
+Environment = autoclass('android.os.Environment')
+File = autoclass('java.io.File')
+Timezone = autoclass('java.util.TimeZone')
+
+# TODO check for storage availibility
+def get_home_folder():
+    kolibri_home_file = File(Environment.getExternalStorageDirectory(), ".kolibri")
+    # prevents content from showing up in things like "gallery"
+    kolibri_home_file.createNewFile(".nomedia")
+    return kolibri_home_file.toString()
+
 
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__), "kolibri", "dist"))
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "kolibri.deployment.default.settings.base"
-os.environ["KOLIBRI_HOME"] = "/data/data/org.le.kolibri/.kolibri"
-os.environ["TZ"] = "America/Los_Angeles"
+os.environ["KOLIBRI_HOME"] = get_home_folder()
+os.environ["TZ"] = Timezone.getDefault().toZoneId()
 
 import django
 django.setup()
